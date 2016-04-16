b23e7b9f5e9c08b0471a01934c251a540353178d
Daltin Loomis <loomisdf@gmail.com>
2016-04-16 18:28:38 -0400

    update variable and value interfaces

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,58 @@
-enum Value {
+#[derive(Debug)]
+pub enum Value {
 	Str(String),
 	Int(i32),
 	Float(f32),
+    Bool(bool),
 	List(Vec<Value>),
 }
 
-struct Variable {
+pub struct Variable {
     value: Value,
     constant: bool,
+}
+
+impl Variable {
+    pub fn new_variable(value: Value) -> Variable {
+        Variable{ value: value, constant: false }
+    }
+
+    pub fn new_constant(value: Value) -> Variable {
+        Variable{ value: value, constant: true }
+    }
+
+    pub fn print(&self) {
+        match &self.value {
+            &Value::Int(i) => print!("{}", i),
+            &Value::Float(i) => print!("{}", i),
+            &Value::Bool(i) => print!("{}", i),
+            &Value::Str(ref i) => print!("{}", i),
+            &Value::List(ref i) => print!("{:?}", i),
+        }
+    }
+
+    pub fn println(&self) {
+        self.print();
+        println!("");
+    }
+
+    pub fn assign(&mut self, value: Value) {
+
+    }
+
+    pub fn add(&self, value: Value) -> Value {
+
+    }
+
+    pub fn subtract(&self, value: Value) -> Value {
+
+    }
+
+    pub fn multipy(&self, value: Value) -> Value {
+
+    }
+
+    pub fn divide(&self, value: Value) -> Value {
+
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate schwift;
+
+use schwift::{ Variable, Value };
+
 fn main() {
-    println!("Hello, world!");
+    let x = Value::Int(5);
+    let y = Variable::new_variable(x);
+    //println!("Hello, {:?}!", x);
+    y.println();
 }
