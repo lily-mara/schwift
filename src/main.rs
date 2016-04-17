c90ef349fe9b59491d6bffb194dec70a6c43c024
Evan Baker <bakerevanj@gmail.com>
2016-04-17 01:01:51 -0400

    test parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 extern crate schwift;
 
-use schwift::{ Variable, Value };
+use schwift::{ Variable, parse_file };
 use schwift::schwift_grammar::{ file };
 
 fn main() {
     //println!("{:?}", statement("x squanch 10"));
     //println!("{:?}", statement("squanch x"));
-    println!("{:?}", file("show me what you got x\n"));
+    println!("{:?}", parse_file("hello.y"));
     //println!("{:?}", assignment("x squanch 10"));
     //println!("{:?}", deletion("squanch x"));
+
 }
