649711a790f41042a3c8e856305bbe756f3e515d
Nate Mara <natemara@gmail.com>
2016-05-23 12:16:34 -0400

    use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 extern crate schwift;
+extern crate clap;
 
-use std::env;
+use clap::{Arg, App};
 
 fn main() {
-    schwift::run_program(&env::args().nth(1).unwrap());
+    let matches = App::new("The Schwift interpreter")
+        .version("0.1")
+        .author("Nate Mara <natemara@gmail.com>")
+        .about("The canonical interpreter for the schwift programming language. Use at your own risk.")
+        .arg(Arg::with_name("SOURCE")
+             .value_name("FILE")
+             .help("The schwift source file that you want to interpret")
+             .takes_value(true)
+             .required(true))
+        .get_matches();
+
+
+    schwift::run_program(matches.value_of("SOURCE").unwrap());
 }
