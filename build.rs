471b55cafe1eb161bd8ed35f9529d954f4203d33
Nate Mara <natemara@gmail.com>
2016-05-29 11:47:38 -0400

    update build.rs

--- build.rs
+++ build.rs
@@ -3,15 +3,21 @@ use std::env;
 use std::path::Path;
 
 fn main() {
-    let dir = env::current_dir().unwrap();
-    let hooks = dir.join(Path::new("hooks"));
-    let hooks_str = hooks.to_str().unwrap();
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("Failed to get cargo manifest directory from env");
+    let manifest_path = Path::new(&manifest_dir);
 
-    let cmd = &format!("rm -rf .git/hooks && ln -s {} .git/hooks", hooks_str);
+    let real_hooks_path = manifest_path.join(Path::new("hooks"));
+    let git_hooks_path = manifest_path.join(".git").join("hooks");
+
+    let real_dir = real_hooks_path.to_str().unwrap();
+    let git_dir = git_hooks_path.to_str().unwrap();
+
+    let cmd = &format!("rm -rf {git} && ln -s {real} {git}", git=git_dir, real=real_dir);
 
     Command::new("sh")
         .arg("-c")
         .arg(cmd)
         .output()
-        .unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
+        .unwrap_or_else(|e| panic!("Failed to create symbolic links: {:?}", e));
 }
